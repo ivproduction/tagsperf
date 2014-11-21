04ec6acefa429cb664b8c6483722222933f699c8
Alex Kirilov <a_kirylau@adamantium.net>
2014-11-21 15:43:59 +0300

    upgraded v4.0 + perf sript 1 + explain + new query

--- tagsPerf/main/groovy/aki/launchable/prod/explain.sql
+++ tagsPerf/main/groovy/aki/launchable/prod/explain.sql
@@ -96,26 +96,26 @@ LIMIT 20, 20;
 
 # links list new
 EXPLAIN SELECT
-  U.CREATION_DATE  AS CREATION_DATE,
-  U.CUSTOM_DOMAIN  AS CUSTOM_DOMAIN,
-  U.CAMPAIGN_NAME  AS CAMPAIGN_NAME,
-  U.HASH           AS HASH,
-  U.TYPE           AS TYPE,
-  U.ORIGINAL_URL   AS ORIGINAL_URL,
-  U.CAMPAIGN_ID    AS CAMPAIGN_ID,
-  U.GEO_LINKS      AS GEO_LINKS,
-  U.UGUID          AS UGUID,
-  U.ALIAS          AS ALIAS,
-  SUM(AGGR.CLICKS) AS CLICKS
-FROM
-  UGUID AS AGGR RIGHT JOIN VIEW_UGUID AS U ON AGGR.UGUID = U.UGUID AND AGGR.DATE BETWEEN '2014-10-13' AND '2014-11-11'
-  JOIN post_master.LINK_TAGS AS lt ON lt.SGUID = U.UGUID
-  JOIN post_master.TAGS AS t ON t.TAG_ID = lt.TAG_ID
-WHERE U.ACCOUNT_ID = 79764 AND U.CAMPAIGN_ARCHIVED = 0 AND U.LINK_ARCHIVED = 0 AND (U.ORIGINAL_URL LIKE '%%') AND
-      t.TAG_NAME = 'monetary'
-GROUP BY U.UGUID
-ORDER BY CLICKS DESC, ORDER_INSERTED DESC
-LIMIT 20, 20;
+                       U.CREATION_DATE  AS CREATION_DATE,
+                       U.CUSTOM_DOMAIN  AS CUSTOM_DOMAIN,
+                       U.CAMPAIGN_NAME  AS CAMPAIGN_NAME,
+                       U.HASH           AS HASH,
+                       U.TYPE           AS TYPE,
+                       U.ORIGINAL_URL   AS ORIGINAL_URL,
+                       U.CAMPAIGN_ID    AS CAMPAIGN_ID,
+                       U.GEO_LINKS      AS GEO_LINKS,
+                       U.UGUID          AS UGUID,
+                       U.ALIAS          AS ALIAS,
+                       SUM(AGGR.CLICKS) AS CLICKS
+                     FROM
+                       UGUID AS AGGR RIGHT JOIN VIEW_UGUID AS U ON AGGR.UGUID = U.UGUID AND AGGR.DATE BETWEEN '2014-10-13' AND '2014-11-11'
+                       JOIN post_master.LINK_TAGS AS lt ON lt.SGUID = U.UGUID
+                       JOIN post_master.TAGS AS t ON t.TAG_ID = lt.TAG_ID
+                     WHERE U.ACCOUNT_ID = 79764 AND U.CAMPAIGN_ARCHIVED = 0 AND U.LINK_ARCHIVED = 0 AND (U.ORIGINAL_URL LIKE '%%') AND
+                           t.TAG_NAME = 'monetary'
+                     GROUP BY U.UGUID
+                     ORDER BY CLICKS DESC, ORDER_INSERTED DESC
+                     LIMIT 20, 20;
 
 # links list new with id
 EXPLAIN SELECT
@@ -283,3 +283,55 @@ EXPLAIN SELECT
         GROUP BY U.UGUID
         ORDER BY CLICKS DESC, ORDER_INSERTED DESC
         LIMIT 20, 20;
+
+
+# v 2.0
+set PROFILING = 1;
+
+EXPLAIN SELECT
+          U.CREATION_DATE  AS CREATION_DATE,
+          U.CUSTOM_DOMAIN  AS CUSTOM_DOMAIN,
+          U.CAMPAIGN_NAME  AS CAMPAIGN_NAME,
+          U.HASH           AS HASH,
+          U.TYPE           AS TYPE,
+          U.ORIGINAL_URL   AS ORIGINAL_URL,
+          U.CAMPAIGN_ID    AS CAMPAIGN_ID,
+          U.GEO_LINKS      AS GEO_LINKS,
+          U.UGUID          AS UGUID,
+          U.ALIAS          AS ALIAS,
+          SUM(AGGR.CLICKS) AS CLICKS
+        FROM
+          UGUID AS AGGR RIGHT JOIN VIEW_UGUID AS U ON AGGR.UGUID = U.UGUID AND AGGR.DATE BETWEEN '2014-10-13' AND '2014-11-11'
+          JOIN post_master.LINK_TAGS AS lt ON lt.SGUID = U.UGUID
+          JOIN post_master.TAGS AS t ON t.TAG_ID = lt.TAG_ID
+        WHERE lt.ACCOUNT_ID= 79764 AND U.CAMPAIGN_ARCHIVED = 0 AND U.LINK_ARCHIVED = 0 AND
+              (U.ORIGINAL_URL LIKE '%%') AND t.TAG_NAME = 'monetary'
+        GROUP BY U.UGUID
+        ORDER BY CLICKS DESC, ORDER_INSERTED DESC
+        LIMIT 20, 20;
+
+
+EXPLAIN SELECT
+          U.CREATION_DATE  AS CREATION_DATE,
+          U.CUSTOM_DOMAIN  AS CUSTOM_DOMAIN,
+          U.CAMPAIGN_NAME  AS CAMPAIGN_NAME,
+          U.HASH           AS HASH,
+          U.TYPE           AS TYPE,
+          U.ORIGINAL_URL   AS ORIGINAL_URL,
+          U.CAMPAIGN_ID    AS CAMPAIGN_ID,
+          U.GEO_LINKS      AS GEO_LINKS,
+          U.UGUID          AS UGUID,
+          U.ALIAS          AS ALIAS,
+          SUM(AGGR.CLICKS) AS CLICKS
+        FROM
+          UGUID AS AGGR RIGHT JOIN VIEW_UGUID AS U ON AGGR.UGUID = U.UGUID AND AGGR.DATE BETWEEN '2014-10-13' AND '2014-11-11'
+          JOIN post_master.LINK_TAGS AS lt ON lt.SGUID = U.UGUID
+          JOIN post_master.TAGS AS t ON t.TAG_ID = lt.TAG_ID
+        WHERE lt.ACCOUNT_ID= 281 AND U.CAMPAIGN_ARCHIVED = 0 AND U.LINK_ARCHIVED = 0 AND
+              (U.ORIGINAL_URL LIKE '%%') AND t.TAG_NAME = 'monetary'
+        GROUP BY U.UGUID
+        ORDER BY CLICKS DESC, ORDER_INSERTED DESC
+        LIMIT 20, 20;
+
+set PROFILING = 0;
+show profiles;
